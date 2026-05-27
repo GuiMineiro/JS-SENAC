da6dba2e6f742d25252650a345f1cb9bab6a7d26
GuilhermeSantos <Guilhermerafaellima@gmail.com>
2026-05-27 20:00:32 -0300

    Update ex5.js

--- ex5.js
+++ ex5.js
@@ -1,4 +1,11 @@
 let peso = 85
 let altura = 1.80
 let imc = peso / (altura * altura)
+if ((imc <= 25)&&(imc > 29.9 )){
+    console.log("Magrelo")
+} else{
+    console.log("Gordin saliente")
+}
+
+
 console.log("Your imc is: "+imc.toFixed(2))
