ccaaa8aeb18f8e944ffaab47634b41b59578b7d8
GuilhermeSantos <Guilhermerafaellima@gmail.com>
2026-05-27 20:02:35 -0300

    Update ex5.js

--- ex5.js
+++ ex5.js
@@ -1,10 +1,10 @@
 let peso = 85
 let altura = 1.80
 let imc = peso / (altura * altura)
-if ((imc <= 25)&&(imc > 29.9 )){
-    console.log("Magrelo")
-} else{
-    console.log("Gordin saliente")
+if ((imc <= 25)&&(imc > 29.9 )){ //testand se o imc é esta entre 25 e 29.9
+    console.log("Magrelo")//caso sim vai escrever "magrelo"
+} else{//caso não
+    console.log("Gordin saliente")// vai escrever "Gordin saliente"
 }
 
 
